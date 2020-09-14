c8e492a588cc42b80cd827f776239d6359b6660f
Augustine Victor <51150248+austinevick@users.noreply.github.com>
2020-09-14 17:19:24 +0100

    added nav bar

--- lib/widgets/content_header.dart
+++ lib/widgets/content_header.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'package:flutter_netflix_ui/models/content_model.dart';
+import 'package:flutter_netflix_ui/widgets/responsive.dart';
+import 'package:video_player/video_player.dart';
 
 import 'vertical_icon_button.dart';
 
@@ -10,6 +12,26 @@ class ContentHeader extends StatelessWidget {
     Key key,
     this.featuredContent,
   }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: ContentHeaderMobile(
+        featuredContent: featuredContent,
+      ),
+      desktop: ContentHeaderDesktop(
+        featuredContent: featuredContent,
+      ),
+    );
+  }
+}
+
+class ContentHeaderMobile extends StatelessWidget {
+  final Content featuredContent;
+  const ContentHeaderMobile({
+    Key key,
+    this.featuredContent,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -62,6 +84,113 @@ class ContentHeader extends StatelessWidget {
               ],
             ))
       ],
+    );
+  }
+}
+
+class ContentHeaderDesktop extends StatefulWidget {
+  final Content featuredContent;
+  const ContentHeaderDesktop({
+    Key key,
+    this.featuredContent,
+  }) : super(key: key);
+
+  @override
+  _ContentHeaderDesktopState createState() => _ContentHeaderDesktopState();
+}
+
+class _ContentHeaderDesktopState extends State<ContentHeaderDesktop> {
+  VideoPlayerController videoPlayerController;
+  bool isMuted = true;
+
+  @override
+  void initState() {
+    videoPlayerController = VideoPlayerController.network(
+      widget.featuredContent.videoUrl,
+    )
+      ..initialize().then((_) => setState(() {}))
+      ..play();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    videoPlayerController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => videoPlayerController.value.isPlaying
+          ? videoPlayerController.pause()
+          : videoPlayerController.play(),
+      child: Stack(
+        alignment: Alignment.bottomLeft,
+        children: [
+          Container(
+            height: 500,
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(
+                      widget.featuredContent.imageUrl,
+                    ))),
+          ),
+          Container(
+            height: 500,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                    colors: [Colors.black, Colors.transparent])),
+          ),
+          Positioned(
+            bottom: 110,
+            child: SizedBox(
+              width: 250,
+              child: Image.asset(
+                widget.featuredContent.titleImageUrl,
+              ),
+            ),
+          ),
+          Positioned(
+              left: 60,
+              right: 60,
+              bottom: 150,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: 250,
+                    child: Image.asset(
+                      widget.featuredContent.titleImageUrl,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    widget.featuredContent.description,
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w500,
+                        shadows: [
+                          Shadow(
+                            blurRadius: 6.0,
+                            color: Colors.black,
+                            offset: Offset(
+                              2,
+                              4,
+                            ),
+                          )
+                        ]),
+                  )
+                ],
+              ))
+        ],
+      ),
     );
   }
 }
